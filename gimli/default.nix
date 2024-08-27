b64b07a3244bd5c8ef78aecef5a6b82013a19869
Santiago Arranz Olmos <santiago.arranz1@gmail.com>
2024-08-27 12:49:31 +0200

    update default.nix

--- gimli/default.nix
+++ gimli/default.nix
@@ -1,6 +1,14 @@
 with import <nixpkgs> {};
 
+let
+  jasmin = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/95c40a6faec32f126c1ffc9b4cf8882689f7a9bc.tar.gz";
+  }) {};
+in
+
 stdenv.mkDerivation {
-  name = "mayo";
-  buildInputs = [ gcc clang-tools valgrind ];
+  name = "gimli";
+  buildInputs = [
+    jasmin.jasmin-compiler gcc clang-tools valgrind
+  ];
 }
