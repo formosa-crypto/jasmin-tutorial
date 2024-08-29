9b5fd457085b815e0d06f029a1013e7a33641c83
Santiago Arranz Olmos <santiago.arranz1@gmail.com>
2024-08-29 19:31:32 +0200

    presentation: add code snippets

--- gimli/jasmin/avx2/jazz_gimli.h
+++ gimli/jasmin/avx2/jazz_gimli.h
@@ -7,9 +7,9 @@ uint32_t jazz_sbox1(uint32_t x, uint32_t y, uint32_t z);
 uint32_t jazz_sbox2(uint32_t x, uint32_t y, uint32_t z);
 uint32_t jazz_sbox3(uint32_t x, uint32_t y, uint32_t z);
 
-uint32_t* jazz_sbox(uint32_t* state, uint64_t column);
-uint32_t* jazz_small_swap(uint32_t* state);
-uint32_t* jazz_big_swap(uint32_t* state);
-uint32_t* jazz_gimli(uint32_t* state);
+void jazz_sbox(uint32_t* state, uint64_t column);
+void jazz_small_swap(uint32_t* state);
+void jazz_big_swap(uint32_t* state);
+void jazz_gimli(uint32_t* state);
 
 #endif
