e92b5f51cc9118ba7093481fa5a5150858aa0fe4
Santiago Arranz Olmos <santiago.arranz1@gmail.com>
2025-03-14 16:33:31 -0300

    restructure

--- tutorial/gimli/c/ref/gimli.h
+++
@@ -1,9 +0,0 @@
-#include <inttypes.h>
-
-uint32_t sbox1(uint32_t x, uint32_t y, uint32_t z);
-uint32_t sbox2(uint32_t x, uint32_t y, uint32_t z);
-uint32_t sbox3(uint32_t x, uint32_t y, uint32_t z);
-uint32_t *sbox(uint32_t *state, uint64_t column);
-uint32_t *small_swap(uint32_t *state);
-uint32_t *big_swap(uint32_t *state);
-void gimli(uint32_t *state);
