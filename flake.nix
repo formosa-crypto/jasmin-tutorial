f0aad8b7a7be5dc064fc4e284f143cef1758b295
Santiago Arranz-Olmos <santiago.arranz1@gmail.com>
2026-02-25 13:50:03 +0100

    flakes

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Jasmin tutorial";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }: {
